a4ae5606196033f815b1d94dc83510bc9f1e1287
kris-Christensen <kristalyn.christensen@gmail.com>
2018-10-03 15:09:51 -0600

    updates
    update w/o pass

---
+++ nightwatch/globals.js
@@ -0,0 +1,11 @@
+module.exports = {
+    
+    gmail:{
+       values:{
+        user1Login: 'kcqa1a@gmail.com',
+        user1Pass: '',
+        user2Login: 'kcqa1b@gmail.com',
+        user2Pass: '',
+       }       
+    },    
+};
